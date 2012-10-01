991072f888f6a24568487278836f1ea17d1af727
David <arkany@gmail.com>
2012-10-01 17:58:44 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+define([], function() {
+  return 'Hello from Yeoman!';
+});
+
+///////////////////////
+// MyScripts
+
+
+$('#myTab a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+})
+
+// for the parallax header, I want to use http://stephband.info/jparallax/ most likely.
+// possibly also this tut, but it is old: http://www.jquery4u.com/animation/jquery-parallax-tutorial/#.UGoCkvl25SM
+
+
